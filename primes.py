19ff4e9a7411b8329856b7f7f1ebb5111d442c7e
Nina Peire <nina.1.peire@kcl.ac.uk>
2022-10-03 20:46:21 +0100

    Complete List of primes v2 Exercise

--- primes.py
+++ primes.py
@@ -1,6 +1,20 @@
 """List of prime numbers generator."""
 """ENTER YOUR SOLUTION HERE!"""
 
+def isPrime(n):
+    isPrime = True
+    for i in range (2,n):
+        if n != 2 and n%i == 0:
+            isPrime = False
+    return isPrime
+
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("You should enter a value greater or equal to 1.")
     list = []
+    n = 2
+    while (len(list) < number_of_primes):
+        if isPrime(n):
+            list.append(n)
+        n += 1
     return list
